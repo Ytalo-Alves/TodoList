99e0037147d1caa6030ca1d455fb364d7e9ac99c
Ytalo-Alves <Ytaloalves10@hotmail.com>
2023-10-16 21:01:38 -0300

    Dockerfile Deploy

---
+++ src/main/java/br/com/alvesytalo/TodoList/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.alvesytalo.TodoList.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private UUID idUser;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+
+    public void setTitle(String title)throws Exception{
+        
+
+        if(title.length() > 50){
+            throw new Exception("O campo title deve ter no maximo 50 caracteres");
+        }
+
+        this.title = title;
+    
+    }   
+}
